c1c734d9e20975451fb2febbb77d576d2693ef32
Chukwuemeka Anyanwu <chrisemeka096@gmail.com>
2022-06-22 23:22:33 +0100

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,34 @@
+const correctAnswers = ['A', 'B', 'B', 'B'];
+const form = document.querySelector('.quiz-form')
+const value = document.querySelector('.result')
+
+
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+
+    let score = 0;
+    const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    // check answer
+    userAnswers.forEach((answer, index) => {
+        if (answer === correctAnswers[index]) {
+            score += 25;
+        }
+    });
+
+    // show result on page
+    scrollTo(0,0)
+    value.classList.remove('d-none')
+
+    let output = 0;
+    const timer = setInterval(() => {
+        value.querySelector('span').textContent = `${output}%`;
+        if (output === score) {
+            clearInterval(timer);
+        }else{
+            output++;
+        }
+    }, 10); 
+
+});
